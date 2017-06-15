6e5d92bfedf701c447b3c9064507ab01c7640a17
wangyao1052 <wangyao1052@163.com>
2017-06-15 22:35:53 +0800

    测试代码

---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LitMath.Vector2 v1 = new LitMath.Vector2(1, 3);
+            //Console.WriteLine(v1.ToString());
+
+            LitMath.Line2 line1 = new LitMath.Line2(
+                new LitMath.Vector2(0, 0),
+                new LitMath.Vector2(10, 0));
+            LitMath.Line2 line2 = new LitMath.Line2(
+                new LitMath.Vector2(5, 0),
+                new LitMath.Vector2(5, 10));
+
+            LitMath.Vector2 intersection = new LitMath.Vector2();
+            if (LitMath.Line2.Intersect(line1, line2, ref intersection))
+            {
+                Console.WriteLine("相交: " + intersection.ToString());
+            }
+        }
+    }
+}
