9e5b9ffda011af0e153a19ee5970ffdf7597c5e8
wangyao <wangyao@threeaxe.com>
2018-08-13 16:34:58 +0800

    1、点是否在多边形内。
    2、线段与射线求交点算法。

--- src/LitMath/Line2.cs
+++ src/LitMath/Line2.cs
@@ -2,6 +2,9 @@
 
 namespace LitMath
 {
+    /// <summary>
+    /// 二维线段
+    /// </summary>
     public struct Line2
     {
         public Vector2 startPoint;
@@ -54,6 +57,7 @@ namespace LitMath
         /// <summary>
         /// 求线段的交点
         /// </summary>
+        /// https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
         /// <param name="line1st">线段1</param>
         /// <param name="line2nd">线段2</param>
         /// <param name="intersection">交点</param>
@@ -61,7 +65,9 @@ namespace LitMath
         ///     true  --- 相交
         ///     false --- 不相交
         /// </returns>
-        public static bool Intersect(Line2 line1st, Line2 line2nd, ref Vector2 intersection,
+        public static bool Intersect(
+            Line2 line1st, Line2 line2nd,
+            ref Vector2 intersection,
             double tolerance = 1e-10)
         {
             Vector2 p = line1st.startPoint;
@@ -75,6 +81,40 @@ namespace LitMath
                 double u = Vector2.Cross(q - p, r) / rxs;
                 if (t >= (0.0-tolerance) && t <= (1.0+tolerance)
                     && u >= (0.0-tolerance) && u <= (1.0+tolerance))
+                {
+                    intersection = p + t * r;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 求线段与射线的交点
+        /// </summary>
+        /// https://stackoverflow.com/questions/14307158/how-do-you-check-for-intersection-between-a-line-segment-and-a-line-ray-emanatin
+        /// <param name="line"></param>
+        /// <param name="rayPnt"></param>
+        /// <param name="rayDir"></param>
+        /// <param name="intersection"></param>
+        /// <param name="tol"></param>
+        /// <returns></returns>
+        public static bool Intersect(
+            Line2 line, Vector2 rayPnt, Vector2 rayDir,
+            ref Vector2 intersection,
+            double tol = 1e-10)
+        {
+            Vector2 p = line.startPoint;
+            Vector2 r = line.endPoint - line.startPoint;
+            Vector2 q = rayPnt;
+            Vector2 s = rayDir;
+            double rxs = Vector2.Cross(r, s);
+            if (!Utils.IsEqualZero(rxs, tol))
+            {
+                double t = Vector2.Cross(q - p, s) / rxs;
+                double u = Vector2.Cross(q - p, r) / rxs;
+                if (t >= (0.0 - tol) && t <= (1.0 + tol)
+                    && u >= (0.0 - tol))
                 {
                     intersection = p + t * r;
                     return true;
---
+++ src/LitMath/Polygon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LitMath
+{
+    /// <summary>
+    /// 多边形
+    /// </summary>
+    public struct Polygon2
+    {
+        /// <summary>
+        /// 点集合
+        /// </summary>
+        private List<Vector2> _pnts;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public Polygon2(List<Vector2> pnts)
+        {
+            _pnts = new List<Vector2>(pnts.Count);
+            _pnts.AddRange(pnts);
+        }
+
+        /// <summary>
+        /// 判断点是否在多边形内
+        /// </summary>
+        public bool IsPointIn(Vector2 pnt)
+        {
+            int count = _pnts.Count;
+            double angle = 0.0;
+            for (int i = 0,j = count - 1; i < count; j = i++)
+            {
+                angle += Vector2.SignedAngle(_pnts[j] - pnt, _pnts[i] - pnt);
+            }
+
+            return !Utils.IsEqualZero(angle, 1e-5);
+
+            #region
+            //int crossing = 0;
+            //int count = _pnts.Count;
+            //Vector2 intersection = new Vector2(double.MaxValue, double.MaxValue);
+            //Vector2 preInter = new Vector2();
+
+            //for (int i = 0, j = count-1; i < count; j=i++)
+            //{
+            //    preInter = intersection;
+            //    Line2 line = new Line2(_pnts[j], _pnts[i]);
+            //    //if (LitMath.Line2.Intersect(line, pnt, new Vector2(1, 1), ref intersection))
+            //    //{
+            //    //    if (!preInter.Equals(intersection))
+            //    //    {
+            //    //        crossing++;
+            //    //    }
+            //    //}
+            //    {
+            //        Vector2 p = line.startPoint;
+            //        Vector2 r = line.endPoint - line.startPoint;
+            //        Vector2 q = pnt;
+            //        Vector2 s = new Vector2(1, 1);
+            //        double rxs = Vector2.Cross(r, s);
+            //        if (!Utils.IsEqualZero(rxs, tol))
+            //        {
+            //            double t = Vector2.Cross(q - p, s) / rxs;
+            //            double u = Vector2.Cross(q - p, r) / rxs;
+            //            if (t >= (0.0 - tol) && t <= (1.0 + tol)
+            //                && u >= (0.0 - tol))
+            //            {
+            //                intersection = p + t * r;
+            //                crossing++;
+            //            }
+            //        }
+            //        else if ()
+            //        {
+
+            //        }
+            //    }
+            //}
+
+            //return (crossing % 2 != 0);
+            #endregion
+        }
+    }
+}
